733f37b7786474f1cfd17727dc36375d5bd8c4bb
Philip M. Gollucci <pgollucci@p6m7g8.com>
2022-08-13 12:11:26 -0400

    chore: Initial Commit.

---
+++ init.zsh
@@ -0,0 +1,28 @@
+# shellcheck shell=bash
+######################################################################
+#<
+#
+# Function: p6df::modules::nmap::deps()
+#
+#>
+######################################################################
+p6df::modules::nmap::deps() {
+	ModuleDeps=(
+		p6m7g8-dotfiles/p6common
+                ohmyzsh/ohmyzsh:plugins/nmap
+	)
+}
+
+######################################################################
+#<
+#
+# Function: p6df::modules::nmap::external::brew()
+#
+#>
+######################################################################
+p6df::modules::nmap::external::brew() {
+
+  brew install nmap
+
+  p6_return_void
+}
